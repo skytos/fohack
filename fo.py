0364752d11ca5a611fcf28f259004eec0fc25dbb
Skylar Broad <skylar.broad@gmail.com>
2021-02-25 16:48:47 -0800

    possibly working descision tree

--- fo.py
+++ fo.py
@@ -1,48 +1,70 @@
 from sys import argv
 
-def dist(a, b):
-    d = 0
+def same(a, b):
+    c = 0
     for i in range(len(a)):
-        if a[i] != b[i]: d += 1
-    return d
+        if a[i] == b[i]: c += 1
+    return c
 
-def dists(words):
-    nwords = len(words)
-    l = len(words[0])
-    ds = [[[] for j in range(l)] for i in range(nwords)]
-    for i in range(nwords):
-        ds[i]
-        for j in range(i+1, nwords):
-            d = dist(words[i], words[j])
-            ds[i][d-1].append(j)
-            ds[j][d-1].append(i)
-    return ds
+def same_classes(words):
+    l = len(words)
+    wl = len(words[0])
+    sc = [{} for i in range(l)]
+    for i in range(len(words)):
+        sc[i][wl] = set((i,))
+        for j in range(i+1, len(words)):
+            s = same(words[i], words[j])
+            if s in sc[i]:
+                sc[i][s].add(j)
+            else:
+                sc[i][s] = set((j,))
+            if s in sc[j]:
+                sc[j][s].add(i)
+            else:
+                sc[j][s] = set((i,))
+    return sc
 
-def best(nwords, l, ds):
-    b, c = None, nwords
-    for i in range(nwords):
-        q = 0
-        for j in range(l):
-            q = max(q, len(ds[i][j]))
-        if q < c:
-            c = q
-            b = i
-    return b, c
-
+def decision_tree(same_classes, kill_depth=5):
+    def rec(possible, depth):
+        if depth >= kill_depth:
+            return kill_depth, None
+        if len(possible) == 1:
+            return depth+1, {'guess': list(possible)[0], 'tree': None}
+        
+        best_depth = kill_depth
+        best_tree = None
+        for i in possible:
+            sc = same_classes[i]
+            max_depth = 0
+            tree = {'guess': i, 'tree': {}}
+            for s in sc:
+                next_possible = possible.intersection(sc[s])
+                if len(next_possible) > 0:
+                    sub_depth, sub_tree = rec(next_possible, depth+1)
+                    max_depth = max(max_depth, sub_depth)
+                    if max_depth >= kill_depth:
+                        break
+                    tree['tree'][s] = sub_tree
+            if max_depth < best_depth:
+                best_depth = max_depth
+                best_tree = tree
+        return best_depth, best_tree
+    
+    return rec(set(range(len(same_classes))), 0)
+    
 def run(words):
-    l = len(words[0])
-    while True:
-        ds = dists(words)
-        b, c = best(len(words), l, ds)
-        d = int(input('%s %d> ' % (words[b], c)))
-        if d == 0:
-            break
-        words = [words[i] for i in ds[b][d-1]]
+    sc = same_classes(words)
+    dt = decision_tree(sc)
+    print(dt)
 
-if len(argv) < 2:
-    print("usage python fo.py <wordfile>")
-    exit()
-file = open('words.txt')
-lines = file.readlines()
-words = list(map(lambda line: line.strip(), lines))
-run(words)
+def main():
+    if len(argv) < 2:
+        print("usage python fo.py <wordfile>")
+        exit()
+    file = open('words.txt')
+    lines = file.readlines()
+    words = list(map(lambda line: line.strip(), lines))
+    run(words)
+
+if __name__ == '__main__':
+    main()
