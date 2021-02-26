5fdc2a8ecb037686e3aaab30b89d5b1d74a96ac0
Skylar Broad <skylar.broad@gmail.com>
2021-02-25 22:32:56 -0800

    fix first guess issue

--- fo.py
+++ fo.py
@@ -24,12 +24,12 @@ def same_classes(words):
                 sc[j][s] = set((i,))
     return sc
 
-def decision_tree(same_classes, kill_depth=5):
-    def rec(possible, depth):
+def decision_tree(same_classes, word_length):
+    def rec(possible, depth, kill_depth):
         if depth >= kill_depth:
             return kill_depth, None
         if len(possible) == 1:
-            return depth+1, {'guess': list(possible)[0], 'tree': None}
+            return depth+1, {'guess': list(possible)[0], 'tree': {word_length: None}}
         
         best_depth = kill_depth
         best_tree = None
@@ -38,29 +38,42 @@ def decision_tree(same_classes, kill_depth=5):
             max_depth = 0
             tree = {'guess': i, 'tree': {}}
             for s in sc:
+                if s == word_length:
+                    tree['tree'][s] = None
+                    continue
                 next_possible = possible.intersection(sc[s])
                 if len(next_possible) > 0:
-                    sub_depth, sub_tree = rec(next_possible, depth+1)
+                    sub_depth, sub_tree = rec(next_possible, depth+1, best_depth)
                     max_depth = max(max_depth, sub_depth)
                     if max_depth >= kill_depth:
                         break
                     tree['tree'][s] = sub_tree
+            tree['tree'][word_length] = None
             if max_depth < best_depth:
                 best_depth = max_depth
                 best_tree = tree
         return best_depth, best_tree
     
-    return rec(set(range(len(same_classes))), 0)
+    return rec(set(range(len(same_classes))), 0, 5)
+
+def test(words, decision_tree):
+    for answer in range(len(words)):
+        print("answer %d" % answer)
+        dt = decision_tree
+        while dt:
+            guess = dt['guess']
+            s = same(words[answer], words[guess])
+            print("guess: %d, same: %d" % (guess, s))
+            dt = dt['tree'][s]
+        print()
     
 def run(words):
     sc = same_classes(words)
-    dt = decision_tree(sc)
-    print(dt)
+    dd, dt = decision_tree(sc, len(words[0]))
+    print(dd, dt)
+    test(words, dt)
 
 def main():
-    if len(argv) < 2:
-        print("usage python fo.py <wordfile>")
-        exit()
     file = open('words.txt')
     lines = file.readlines()
     words = list(map(lambda line: line.strip(), lines))
